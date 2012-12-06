11da48a0cec8fca8aa576b6514acacf718d7a615
aelaa <aelaau@gmail.com>
2012-12-06 13:50:59 +0400

    Login troubles manual added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -49,3 +49,17 @@ function show_full(el) {
         el.style.wordWrap = "";
     };
 }
+
+function show_login_help() {
+    document.getElementById('black').style.display = "block";
+    help_win = document.getElementById('login_help');
+    help_win.style.opacity = 1;
+    help_win.style.display = 'block';
+}
+
+function hide_login_help() {
+    document.getElementById('black').style.display = "none";
+    document.body.style.backgroundColor = "";
+    help_win = document.getElementById('login_help');
+    help_win.style.display = 'none';
+}
