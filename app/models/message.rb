ca5b64d954a8eefa6c851da4146e7cda8007d3e2
aelaa <aelaau@gmail.com>
2013-01-14 16:26:25 +0400

    master cleaning

--- app/models/message.rb
+++
@@ -1,5 +0,0 @@
-# -*- encoding : utf-8 -*-
-class Message < ActiveRecord::Base
-  attr_accessible :message
-  belongs_to :social_user
-end
--- config/initializers/secret_token.rb
+++
@@ -1,8 +0,0 @@
-# -*- encoding : utf-8 -*-
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-UlgoodRv5::Application.config.secret_token = 'a4f8163f3c94465b06c5902cbf992d68d1c8cc1ffb6c91628deaa4f5b6995115a2c6779daab4d3838afed3f866561df136f39d558763e46efd6c5847007a5bf9'
