759b7f1c5f4797c35faf65f0be8e798876a0e542
aelaa <aelaau@gmail.com>
2013-01-27 18:44:29 +0400

    auto update - IN PROCESS!!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,25 +1,4 @@
 # -*- encoding : utf-8 -*-
 module ApplicationHelper
 
-  def counters
-    %{
-    <div class="counters" style="display:inline-block;">
-      <div style="text-align:center;">Добрых дел</div>
-      <div class="simple_counter">
-        Сегодня:
-          <div class="counter_number">
-    #{link_to (Message.where("created_at >= :time AND checked != 0", :time => Time.now.midnight).count),  {:controller => "main", :action => "home"}, :remote => true}
-          </div>
-      </div>
-    <div style="width:10px; display: inline-block;"></div>
-    <div class="simple_counter">
-    Всего:
-    <div class="counter_number">
-    #{link_to Message.where("checked != 0").count, {:controller => "main", :action => "home", :messages => "all"}, :remote => true}
-    </div>
-    </div>
-        </div>
-}
-  end
-
 end
