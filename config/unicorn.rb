33e31795a6cf96a27fd4d50bf6e8e02719ee31c4
Shakirov Ilya <aelaau@gmail.com>
2013-12-06 16:40:56 +0400

    Unicorn config added

---
+++ config/unicorn.rb
@@ -0,0 +1,28 @@
+APP_PATH = "/srv/skydance"
+
+worker_processes 1
+
+working_directory APP_PATH + "/current" # available in 0.94.0+
+
+listen APP_PATH + "/shared/.sock", backlog: 64
+
+timeout 30
+
+pid APP_PATH + "/shared/pids/unicorn.pid"
+
+stderr_path APP_PATH + "/shared/log/unicorn.stderr.log"
+stdout_path APP_PATH + "/shared/log/unicorn.stdout.log"
+
+preload_app true
+GC.respond_to?(:copy_on_write_friendly=) and
+    GC.copy_on_write_friendly = true
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.establish_connection
+end
